78f60a32c16d173532aef4efa801b4554cf19623
cjbaezilla <carlos.baeza@gmail.com>
2026-03-22 12:50:57 -0300

    feat: add max supply limit to BaseErc721PropertyNFT contract
    Introduce immutable maxSupply parameter to enforce supply cap on fractional real estate NFTs. The constructor now accepts _maxSupply parameter which is stored as an immutable state variable. The safeMint function includes a require statement to prevent exceeding the maximum supply, protecting against over-minting.

--- hardhat2/contracts/BaseErc721PropertyNFT.sol
+++ hardhat2/contracts/BaseErc721PropertyNFT.sol
@@ -7,13 +7,17 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BaseErc721PropertyNFT is ERC721, Ownable {
     uint256 private _nextTokenId;
+    uint256 public immutable maxSupply;
 
-    constructor(address initialOwner, string memory name, string memory ticker)
+    constructor(address initialOwner, string memory name, string memory ticker, uint256 _maxSupply)
         ERC721(name, ticker)
         Ownable(initialOwner)
-    {}
+    {
+        maxSupply = _maxSupply;
+    }
 
     function safeMint(address to) public onlyOwner returns (uint256) {
+        require(_nextTokenId < maxSupply, "Max supply exceeded");
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
         return tokenId;
